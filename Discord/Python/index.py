ea4855caa7e69700988030f116a385d085987d38
Ken Bacchiani <bacchiani@wisc.edu>
2021-03-18 12:42:55 -0500

    Initial Bot Setup

---
+++ Discord/Python/index.py
@@ -0,0 +1,18 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!help'):
+        await message.channel.send('Commands:')
+
+client.run(os.getenv('TOKEN'))
