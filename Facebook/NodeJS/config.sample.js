f0f44241807cef80d803efbb85dcdd207b0e75e7
healzer <justdoitzlol@gmail.com>
2021-03-28 10:30:57 +0000

    spurwing api added

--- Facebook/NodeJS/config.sample.js
+++ Facebook/NodeJS/config.sample.js
@@ -2,4 +2,6 @@ module.exports = {
   APP_SECRET : '',
   ACCESS_TOKEN: '',
   CALLBACK_USER_TOKEN: '', // custom
+  PID: '',
+  APTID: '',
 }
--- Facebook/NodeJS/index.js
+++ Facebook/NodeJS/index.js
@@ -10,8 +10,10 @@ const config = require('./config.js')
 const APP_SECRET = config.APP_SECRET;
 const ACCESS_TOKEN = config.ACCESS_TOKEN;
 const CALLBACK_USER_TOKEN = config.CALLBACK_USER_TOKEN;
+const PID = config.PID; // Spurwing provider id
+const APTID = config.APTID; // Spurwing appointment type id from: sp.get_appointment_types(PID)
 
-if (!APP_SECRET.length || !ACCESS_TOKEN.length || !CALLBACK_USER_TOKEN.length)
+if (!APP_SECRET.length || !ACCESS_TOKEN.length || !CALLBACK_USER_TOKEN.length || !PID.length || !APTID.length)
   throw 'invalid config values';
 
 app.use(cors())
@@ -33,12 +35,13 @@ router.get('/spurwing-fbbot/', (req, res) => {
     }
 });
 
-router.post('/spurwing-fbbot/', (req, res) => {
+router.post('/spurwing-fbbot/', async (req, res) => {
   verifyRequestSignature(req, res)
+  // console.log(JSON.stringify(req.body))
   for (const e of req.body.entry) {
     if (e.messaging)
       for (const m of e.messaging) {
-        fb_msg_process(m.sender.id, m.message)
+        await fb_msg_process(m.sender.id, m.message)
       }
   }
   res.send({success:1})
@@ -57,24 +60,97 @@ function verifyRequestSignature(req, res) {
     throw new Error("Invalid signature.");
 }
 
-function fb_msg_process(senderId, msg) {
-  console.log(msg) // text, quick_reply.payload
+const Spurwing = require('spurwing');
+const moment = require('moment');
+let sp = new Spurwing();
+
+async function fb_msg_process(senderId, msg) {
+  // console.log(msg) // text, quick_reply.payload
+  
   let resp = {text: "I don't know what you mean :("}
-  if (msg && msg.text && msg.text.toLowerCase() === "start") {
-    resp = {
-      text: "What's your choice?",
-      quick_replies:[{
-        content_type: "text",
-        title: "Today",
-        payload: "answer:0",
-      },{
-        content_type: "text",
-        title: "Tomorrow",
-        payload: "answer:1",
-      }]
-    }
+
+  if (msg && msg.text) {
+    let text = msg.text.toLowerCase();
+    if (msg.quick_reply && msg.quick_reply.payload)
+      text = msg.quick_reply.payload;
+
+    switch(true) {
+      case /^book$/.test(text):
+        resp = await fb_msg_process_funcs.book(text);
+        break;
+      case /^book_day:.+$/.test(text):
+        resp = await fb_msg_process_funcs.book_day(text);
+        break;
+      case /^book_slot:.+$/.test(text):
+        resp = await fb_msg_process_funcs.book_slot(text);
+        break;
+    }  
   }
+
   fb_msg_reply(senderId, resp)
+}
+const fb_msg_process_funcs = {
+
+  // all dates are relative to server's timezone
+  // the user may be in a different timezone
+  // facebook can provide user timezone: under advanced messaging features
+
+  book: async function(text) {
+    const days = [];
+    let B = await sp.get_days_available(PID, APTID, moment().format('YYYY-MM-DD'));
+    days.push(...B.days_available.slice(0, 3));
+    if (days.length < 3) {
+      B = await sp.get_days_available(PID, APTID, moment().add(1, 'months').format('YYYY-MM-DD'));
+      days.push(...B.days_available.slice(0, 3-days.length ));
+    }
+    const quick_replies = [];
+    for (const day of days) {
+      quick_replies.push({
+        content_type: "text",
+        title: day,
+        payload: "book_day:" + day,
+      })
+    }
+    return {
+      text: "When would you like to book?",
+      quick_replies: quick_replies,
+    }
+  },
+
+  book_day: async function(text) {
+    const answer = text.split('book_day:')[1];
+    let C = await sp.get_slots_available(PID, APTID, answer, answer)
+    if (! C.slots_available.length) {
+      return {text: 'There are no time slots available on that day :/'};
+    }
+    const quick_replies = [];
+    for (const slot of C.slots_available.slice(0,5)) {
+      quick_replies.push({
+        content_type: "text",
+        title: moment(slot.date.replace(' +', '+').replace(' -', '-')).format('hh:mm A'),
+        payload: "book_slot:" + slot.date,
+      })
+    }
+    return {
+      text: "Select your preferred time slot:",
+      quick_replies: quick_replies,
+    }
+  },
+
+  book_slot: async function(text) {
+    const answer = text.split('book_slot:')[1];    
+    try {
+      await sp.complete_booking(PID, APTID,
+          'ilya_test@nevolin.be', 'Ilya', 'Nevo',
+          answer,
+          'Test Facecbook booking'
+        );
+      return { text: "Success!" }
+    } catch (err) {
+      console.log(err)
+      return { text: "Error :( " }
+    }
+  },
 }
 
 function fb_msg_reply(senderId, response) {
