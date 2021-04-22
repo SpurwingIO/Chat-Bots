dbf86a9cb3b522c9dfd4d83855ad85da739f19c3
Ilya Nevolin <justdoitzlol@gmail.com>
2021-04-22 16:46:39 +0200

    spurwing & basic commands implemented

--- Discord/NodeJS/config.sample.js
+++ Discord/NodeJS/config.sample.js
@@ -1,3 +1,5 @@
 module.exports = {
-  token: "",
+  token: '',
+  PID: '',
+  APTID: '',
 }
--- Discord/NodeJS/index.js
+++ Discord/NodeJS/index.js
@@ -1,41 +1,116 @@
-const Discord = require('discord.js');
-const config = require('./config.js')
-const chrono = require('chrono-node');
+const Discord   = require('discord.js');
+const config    = require('./config.js');
+const chrono    = require('chrono-node');
+const Spurwing  = require('spurwing');
+const moment    = require('moment-timezone');
+
+const sp = new Spurwing();
+let APTID = config.APTID; // appointment type id
+if (!APTID) {
+  // load random aptid if none provided
+  sp.get_appointment_types(config.PID).then(arr => {
+    APTID=arr[0].id; // dynamically select some appointment type
+    console.log(arr[0]);
+  });
+}
 
 const client = new Discord.Client(); 
 const prefix = '!';
 
-//event listeners based on client 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 });
 
-const rBook = /^book (.+)$/;        // arg: date + time
-const rDaysAvail = /^days (.+)$/;   // arg: month (of date)
-const rSlotsAvail = /^slots (.+)$/; // arg: day (of date)
+// regular expressions for commands
+const rBook = /^book (.+)$/;        // arg: datetime + email
+const rDaysAvail = /^days( .+)?$/;   // arg: month (of date)
+const rSlotsAvail = /^slots( .+)?$/; // arg: day (of date)
+const rEmail = /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
 
-client.on('message', function(msg) {
+client.on('message', async function(msg) {
   if (msg.author.bot) return;
   if (!msg.content.startsWith(prefix)) return;
 
   const cmd = msg.content.slice(prefix.length);
+  let reply;
   switch(true) {
     case rDaysAvail.test(cmd):
-      let arg = cmd.match(rDaysAvail)[1];
-      let dt = chrono.parseDate(arg);
-      if (!dt) return msg.reply('Unable to interpret date from: ' + arg);
-      console.log(dt)
-      msg.reply("dt:"+ dt)
+      reply = await fDaysAvail(cmd)
       break;
     case rSlotsAvail.test(cmd):
+      reply = await fSlotsAvail(cmd)
       break;
     case rBook.test(cmd):
+      reply = await fBook(cmd, msg.author.username)
       break;
     default:
-      msg.reply('Invalid command, unable to process.')
+      reply = ('Invalid command, unable to process.')
       break;
   }
-  // message.reply
+  msg.reply('\n```' + reply + '```')
 });
 
+async function fBook(cmd, name) {
+  cmd = cmd.match(rBook)[1];
+  if (!rEmail.test(cmd)) return ('Missing or invalid email address');
+  let args = cmd.match(/^(.+) (.+?@.+)$/);
+  if (!args) return ('Missing date');
+  let dt = chrono.parseDate(args[1])
+  if (!dt) return ('Unable to interpret date from: ' + args[1]);
+  let email = args[2].trim().toLowerCase();
+  let out;
+  try {
+    out = await sp.complete_booking(config.PID, APTID, email, name, '-', dt, 'Discord Appointment')
+    return 'wohoo!' + `\n\nSlot: ${moment(dt.getTime()).format('YYYY-MM-DD HH:mm Z')}`
+  } catch (err) {
+    if (err && err.response && err.response.data)
+      return 'Error:\n' + err.response.data.message + `\n\nSlot: ${moment(dt.getTime()).format('YYYY-MM-DD HH:mm Z')}`
+  }
+}
+async function fDaysAvail(cmd) {
+  cmd = cmd.match(rDaysAvail)[1];
+  if (!cmd) cmd = (new Date()).toISOString();
+  let dt = chrono.parseDate(cmd);
+  if (!dt) return ('Unable to interpret date from: ' + cmd);
+  const days = await sp.get_days_available(config.PID, APTID, dt);
+  if (days.days_available && days.days_available.length)
+    return days.days_available.join('\n')
+  else
+    return 'no days available that month'
+}
+async function fSlotsAvail(cmd) {
+  cmd = cmd.match(rSlotsAvail)[1];
+  if (!cmd) cmd = (new Date()).toISOString();
+  let dt = chrono.parseDate(cmd);
+  if (!dt) return ('Unable to interpret date from: ' + cmd);
+  let dtx = chrono.parse(cmd);
+  let tz = null;
+  if (dtx[0].start && dtx[0].start.knownValues && dtx[0].start.knownValues.timezoneOffset) {
+    let offset = -dtx[0].start.knownValues.timezoneOffset;
+    tz = tzMap.get(offset)
+    tz = tz.values().next().value
+  }
+  const slots = await sp.get_slots_available(config.PID, APTID, dt, dt, false, tz);
+  if (slots.slots_available && slots.slots_available.length)
+    return slots.slots_available.map(x=>x.date).join('\n')
+  else
+    return 'no slots available that day'
+}
+
+const tzMap = new Map(); // offset to timezone name mapping
+for (const name of moment.tz.names()) {
+  const offsets = moment.tz.zone(name).offsets;
+  
+  for (const offset of offsets) {
+      if (!tzMap.has(offset)) {
+          tzMap.set(offset, new Set());
+      }
+      tzMap.get(offset).add(name);
+  }
+}
+
+
+// fDaysAvail('days april 22').then(console.log);
+// fSlotsAvail('slots tomorrow 10am edt').then(console.log);
+// fBook('book 2021-04-23 03:00:00 -0400 ilya@nevolin.be').then(console.log)
 client.login(config.token);
